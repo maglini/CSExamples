d6f040cf2d01a59b63eca35bba2d2711cabe73e6
Vladyslava <alex.maglini@gmail.com>
2022-03-20 16:06:10 +0200

    Realized examples with Union, Distinct, Except, Intersect

---
+++ Logic/Compares/UniqueGenericComparer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Logic.Compares
+{
+    public class UniqueGenericComparer<T> : IEqualityComparer<T> where T : class
+    {
+        private readonly IEnumerable<Func<T, object>> _funcs;
+
+        public UniqueGenericComparer(IEnumerable<Func<T, object>> funcs)
+        {
+            _funcs = funcs;
+        }
+        
+        public UniqueGenericComparer(Func<T, object> func)
+        {
+            _funcs = new List<Func<T, object>>() {func};
+        }
+   
+        public bool Equals(T ? t1, T? t2)
+        {
+            bool result = t1!= null && t2 != null;
+            
+            foreach (var func in _funcs)
+            {
+                result &= func.Invoke(t1) == func.Invoke(t2);
+            }
+            
+            return result;
+        }
+
+        public int GetHashCode(T t)
+        {
+            var hash = 0;
+            foreach (var func in _funcs)
+            {
+                var property = func.Invoke(t);
+                hash += property.GetHashCode();
+            }
+
+            return hash;
+        }
+    }
+}
---
+++ Tests/LogicByReferenceTest.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Logic;
+using Logic.Models;
+using Xunit;
+
+namespace Tests
+{
+    public class LogicByReferenceTest
+    {
+        private User User9 = new User() {Id = 9, Name = "Vladyslav", Username = "user9"};
+        private User User10 = new User() {Id = 10, Name = "Vladymir", Username = "user10"};
+        
+        [Fact]
+        public void UnionTest()
+        {
+            // ARRANGE
+            var setC = new List<User>()
+            {
+                User9,
+                User9,
+                User10
+            };
+
+            var setD = new List<User>()
+            {
+                User9,
+                User9,
+            };
+
+            var expected = new List<User>() {User9, User10};
+
+            // ACT
+            var actual = setC.UnionByReference(setD);
+
+            // ASSERT
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void DistinctTest()
+        {
+            // ARRANGE
+            var setC = new List<User>()
+            {
+                User9,
+                User9,
+                User10
+            };
+
+            var expected = new List<User>() {User9, User10};
+
+            // ACT
+            var actual = setC.DistinctByReference();
+
+            // ASSERT
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void ExceptTest()
+        {
+            // ARRANGE
+            var setC = new List<User>()
+            {
+                User9,
+                User9,
+                User10
+            };
+
+            var setD = new List<User>()
+            {
+                User9,
+                User9,
+            };
+
+            var expected = new List<User>() {User10};
+
+            // ACT
+            var actual = setC.ExceptByReference(setD);
+
+            // ASSET
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void IntersectTest()
+        {
+            // ARRANGE
+            var setC = new List<User>()
+            {
+                User9,
+                User9,
+                User10
+            };
+
+            var setD = new List<User>()
+            {
+                User9,
+                User9,
+            };
+
+            var expected = new List<User>() {User9};
+
+            // ACT
+            var actual = setC.IntersectByReference(setD);
+
+            // ASSET
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
