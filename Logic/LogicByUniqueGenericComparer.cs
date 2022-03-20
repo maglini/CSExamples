60a241feafeea08292609f1fe8120c9560fb4490
Vladyslava <alex.maglini@gmail.com>
2022-03-21 00:31:06 +0200

    ignore some files

---
+++ Logic/LogicByUniqueGenericComparer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Logic
+{
+    public static class LogicByUniqueGenericComparer
+    {
+        public static IEnumerable<T> UnionByComparer<T>(
+            this IEnumerable<T> first, IEnumerable<T> second, IEqualityComparer<T> comparer)
+        {
+            return first.Union(second, comparer);
+        }
+        
+        public static IEnumerable<T> DistinctByComparer<T>(this IEnumerable<T> first, IEqualityComparer<T> comparer)
+        {
+            return first.Distinct(comparer);
+        }
+        
+        public static IEnumerable<T> ExceptByComparer<T>(
+            this IEnumerable<T> first, IEnumerable<T> second, IEqualityComparer<T> comparer)
+        {
+            return first.Except(second, comparer);
+        }
+
+        public static IEnumerable<T> IntersectByComparer<T>(
+            this IEnumerable<T> first, IEnumerable<T> second, IEqualityComparer<T> comparer)
+        {
+            return first.Intersect(second, comparer);
+        }
+    }
+}
---
+++ Logic/Models/Cat.cs
@@ -0,0 +1,12 @@
+﻿namespace Logic.Models
+{
+    public class Cat
+    {
+        public int Id { get; init; }
+        
+        // Would be unique
+        public string Name { get; init; }
+        
+        public string Color { get; init; }
+    }
+}
